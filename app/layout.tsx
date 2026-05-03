b7e0a8c307dfb42cfebc4f40ce9178934ca09f59
Mohanad Ayoub <alaqlmhnd3@gmail.com>
2026-05-03 16:25:50 +0300

    added clerk tailwind layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
 			<body className="min-h-full flex flex-col">
 				<ClerkProvider
 					appearance={{
+						cssLayerName: "clerk",
 						theme: dark,
 						variables: {
 							colorPrimary: "var(--accent-primary)",
